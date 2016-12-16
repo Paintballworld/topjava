20c8f2163bf469228760e11f358ab44558896389
PaintballWorld <PaintballWorld@rambler.ru>
2016-12-16 08:02:08 +0600

    Applied all 2-x patches

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,7 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.repository.InMemoryMealRepositoryImpl;
+import ru.javawebinar.topjava.repository.mock.InMemoryMealRepositoryImpl;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
@@ -21,7 +21,8 @@ import java.util.Objects;
  * User: gkislin
  * Date: 19.08.2014
  */
-public class MealServlet extends HttpServlet {
+public class  MealServlet extends HttpServlet {
+
     private static final Logger LOG = LoggerFactory.getLogger(MealServlet.class);
 
     private MealRepository repository;
