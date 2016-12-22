f73640b8a43a5d9bacef2514069689a78100333e
PaintballWorld <PaintballWorld@rambler.ru>
2016-12-22 17:00:11 +0600

    Applied 3_07 patch. No errors

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,10 +1,15 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.AuthorizedUser;
+
+import org.slf4j.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * User: gkislin
@@ -12,8 +17,17 @@ import java.io.IOException;
  */
 public class UserServlet extends HttpServlet {
 
+    private static final Logger LOG = getLogger(UserServlet.class);
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        AuthorizedUser.setId(userId);
+        response.sendRedirect("meals");
+    }
+
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+        LOG.debug("forward to users");
     }
 }
