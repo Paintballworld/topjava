d026027c4b62ff625aef5e80641f1ea7cbdf0310
PaintballWorld <PaintballWorld@rambler.ru>
2016-12-26 19:29:07 +0600

    Edited DB population

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -7,6 +8,34 @@ VALUES ('User', 'user@yandex.ru', 'password');
 
 INSERT INTO users (name, email, password)
 VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Завтрак для админа', 100000, '2016-12-24 06:00:00', 500);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Обед для админа', 100000, '2016-12-24 12:00:00', 750);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Ужин для админа', 100000, '2016-12-24 18:00:00', 500);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Завтрак - доширак', 100001, '2016-12-24 06:00:00', 150);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Обед - Утка по пекински', 100001, '2016-12-24 12:00:00', 800);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Ужин - Мясо по-тайски', 100001, '2016-12-24 19:10:00', 1250);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Завтрак - ямчница', 100001, '2016-12-25 06:00:00', 150);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Обед - Роллы', 100001, '2016-12-25 12:00:00', 800);
+
+INSERT INTO meals (description, user_id, date_time, calories)
+VALUES ('Ужин - Пюре с котлетой', 100001, '2016-12-25 19:10:00', 1250);
+
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
