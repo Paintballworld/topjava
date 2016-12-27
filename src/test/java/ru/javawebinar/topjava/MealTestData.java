06399b7edf2242368b51706f4c7455b5b2534fd8
PaintballWorld <PaintballWorld@rambler.ru>
2016-12-27 19:34:35 +0600

    MAde default constructor for Meal class, also added setters for JdbcTemplate

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,12 +3,24 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 /**
  * GKislin
  * 13.03.2015.
  */
 public class MealTestData {
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final int ADMIN_ID = START_SEQ;
+    public static final int USER_ID = START_SEQ + 1;
+
+    public static final ModelMatcher<Meal> MATCHER_DEFAULT = new ModelMatcher<>();
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                    (expected.toString().equalsIgnoreCase(actual.toString()))
+    );
+
+
 
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,12 +1,19 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.DbPopulator;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+
 import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 
 /**
  * Created by yerlan on 12/27/16.
@@ -24,6 +31,16 @@ public class MealServiceTest {
     @Autowired
     private DbPopulator dbPopulator;
 
+    @Before
+    public void setUo() throws Exception {
+        dbPopulator.execute();
+    }
 
+    @Test
+    public void testSave() throws Exception {
+        Meal newMeal = new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+        Meal meal = service.save(newMeal, ADMIN_ID);
+
+    }
 
 }
