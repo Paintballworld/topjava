8cf1dc5ac4cbd78d14a54acc3b53c4b3e77c7bb2
Yerlan <Paintballworld@rambler.ru>
2017-01-20 15:41:36 +0600

    Applied all 6_* patches. Ready to solve HW6

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,8 +1,13 @@
 package ru.javawebinar.topjava.web;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import org.slf4j.Logger;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +24,15 @@ public class UserServlet extends HttpServlet {
 
     private static final Logger LOG = getLogger(UserServlet.class);
 
+    private AdminRestController adminController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        adminController = springContext.getBean(AdminRestController.class);
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int userId = Integer.valueOf(request.getParameter("userId"));
         AuthorizedUser.setId(userId);
@@ -28,6 +42,7 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("forward to users");
+        LOG.debug("getAll");
+        request.setAttribute("users", adminController.getAll());
     }
 }
